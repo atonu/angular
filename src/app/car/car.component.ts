9e838727895f51c9a9b5a93752814051cecfbd48
atonu <atonu.zahin@gmail.com>
2018-09-13 16:31:31 +0600

    2

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from '../car';
 import { CARS } from '../mock-cars';
+import { CarService } from '../car.service';
 
 @Component({
   selector: 'app-car',
@@ -14,16 +15,21 @@ export class CarComponent implements OnInit {
     name: 'Audi'
   };
 
-  cars=CARS;
+  cars : Car[];
 
   selectedCar: Car;
 
   Menu = "close";
 
-  constructor() { }
+  constructor(private carService: CarService) { }
 
   ngOnInit() {
-  }
+  this.getCars();
+}
+
+  getCars(): void {
+  this.cars = this.carService.getCars();
+}
 
   onSelect(car: Car): void {
   this.selectedCar = car;
